c325b715cbf4be4b16b32dfb92e6248de6259e1b
JuShapiro <yuliashepel19@gmail.com>
2024-06-06 12:26:55 +0200

    createLoader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { Oval } from "react-loader-spinner";
+import css from "./Loader.module.css";
+
+const Loader = () => {
+  return (
+    <div className={css.backdrop}>
+      <Oval
+        visible={true}
+        height="80"
+        width="80"
+        color="grey"
+        ariaLabel="oval-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
+
+export default Loader;
